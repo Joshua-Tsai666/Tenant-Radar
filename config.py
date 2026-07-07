d769d6947e2ddd034443d1f2736406b87e7d1498
Joshua-Tsai666 <fluo.joshua@gmail.com>
2026-07-07 11:19:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,41 @@ OPENAI_API_KEY = "your-openai-api-key-here"
 
 # Facebook 相關設定
 FB_COOKIE_PATH = "fb_cookies.json"  # 瀏覽器匯出的 Cookie 檔案路徑
-FB_GROUP_URL = "https://facebook.com"  # 您想監控的社團網址
+FB_GROUP_URLS = [
+    "https://www.facebook.com/groups/2391145197642950",
+    "https://www.facebook.com/groups/464870710346711",
+    "https://www.facebook.com/groups/459966811445588",
+    "https://facebook.com/groups/313385739282042",
+    "https://www.facebook.com/groups/227082894440964",
+    "https://www.facebook.com/groups/359576301158357",
+    "https://www.facebook.com/groups/1040396368050817",
+    "https://www.facebook.com/groups/1513936138926333",
+    "https://www.facebook.com/groups/939218550146090",
+    "https://www.facebook.com/groups/3140810842843485",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    "https://facebook.com",
+    
+]
+# 您監控的社團網址
